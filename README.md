49ccb81808bc8ed0e9c52827b2373a4ebae7e2a9
skkim-01 <lemoned222@gmail.com>
2023-07-12 06:35:26 +0000

    Update: Change path layout - TODO: Bootstrap version bugfix

--- README.md
+++ README.md
@@ -10,3 +10,45 @@ The `cdk.json` file tells the CDK toolkit how to execute your app.
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
  * `go test`         run unit tests
+
+
+### added by me
+* `cdk destroy`     destroy stack
+
+
+### Issues
+##### cdk synth issue
+```
+test@test:~/src/go-cdk/test$ cdk synth
+Bundling asset CDKSVRLESSGOSTACK/cdk-go-lambds/Code/Stage...
+
+#1 [internal] load .dockerignore
+#1 transferring context: 2B done
+#1 DONE 0.0s
+
+#2 [internal] load build definition from Dockerfile
+#2 transferring dockerfile: 1.30kB 0.0s done
+#2 DONE 0.0s
+
+...
+
+Parameters:
+  BootstrapVersion:
+    Type: AWS::SSM::Parameter::Value<String>
+    Default: /cdk-bootstrap/hnb659fds/version
+    Description: Version of the CDK Bootstrap resources in this environment, automatically retrieved from SSM Parameter Store. [cdk:skip]
+Rules:
+  CheckBootstrapVersion:
+    Assertions:
+      - Assert:
+          Fn::Not:
+            - Fn::Contains:
+                - - "1"
+                  - "2"
+                  - "3"
+                  - "4"
+                  - "5"
+                - Ref: BootstrapVersion
+        AssertDescription: CDK bootstrap stack version 6 required. Please run 'cdk bootstrap' with a recent version of the CDK CLI.
+```
+* https://github.com/aws/aws-cdk/issues/17942
--- cdkmain.go
+++ cdkmain.go
@@ -2,71 +2,15 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
-	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
-	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/aws/jsii-runtime-go"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awslambdanodejs" // for node handler
+	
+	"go-cdk-test/stacks/lambda_api"
 )
-
-type AppServerlessCdkGoStackProps struct {
-	awscdk.StackProps
-}
-
-func NewAppServerlessCdkGoStack(scope constructs.Construct, id string, props *AppServerlessCdkGoStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
-
-	// create Lambda function
-	getHandler := awscdklambdagoalpha.NewGoFunction(stack, jsii.String("cdk-go-lambds"), &awscdklambdagoalpha.GoFunctionProps{
-		Runtime: awslambda.Runtime_GO_1_X(),
-		Entry:   jsii.String("./lambdasfn/go-hello"),
-		Bundling: &awscdklambdagoalpha.BundlingOptions{
-			GoBuildFlags: jsii.Strings(`-ldflags "-s -w"`),
-		},
-	})
-
-	// node lambda function
-	nodeHandler := awslambdanodejs.NewNodejsFunction(stack, jsii.String("cdk-node-lambds"), &awslambdanodejs.NodejsFunctionProps{
-		Runtime: awslambda.Runtime_NODEJS_14_X(),
-		// insert specific js/ts file for entry
-		Entry:   jsii.String("./lambdasfn/node-hello/index.js"),
-		// need dependency lock file
-		DepsLockFilePath: jsii.String("./lambdasfn/node-hello/package-lock.json"),
-	})
-
-	// API Gateway name
-	restApi := awsapigateway.NewRestApi(stack, jsii.String("cdkgoapi"), &awsapigateway.RestApiProps{
-		RestApiName:    jsii.String("cdkgoapi"),
-		CloudWatchRole: jsii.Bool(false),
-	})
-
-	// Create APIGateway RestAPI: go handler
-	restApi.Root().AddResource(jsii.String("go-hello"), &awsapigateway.ResourceOptions{}).AddMethod(
-		jsii.String("GET"),
-		awsapigateway.NewLambdaIntegration(getHandler, &awsapigateway.LambdaIntegrationOptions{}),
-		restApi.Root().DefaultMethodOptions(),
-	)
-
-	// Create APIGateway RestAPI: node handler
-	restApi.Root().AddResource(jsii.String("node-hello"), &awsapigateway.ResourceOptions{}).AddMethod(
-		jsii.String("GET"),
-		awsapigateway.NewLambdaIntegration(nodeHandler, &awsapigateway.LambdaIntegrationOptions{}),
-		restApi.Root().DefaultMethodOptions(),
-	)
-
-	return stack
-}
 
 func main() {
 	app := awscdk.NewApp(nil)
 
 	// CloudFormation stack name
-	NewAppServerlessCdkGoStack(app, "CDKSVRLESSGOSTACK", &AppServerlessCdkGoStackProps{
+	lambda_api.NewAppServerlessCdkGoStack(app, "CDKSVRLESSGOSTACK", &lambda_api.AppServerlessCdkGoStackProps{
 		awscdk.StackProps{
 			Env: env(),
 		},
